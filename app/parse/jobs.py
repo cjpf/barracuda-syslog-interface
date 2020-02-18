65fc69aee7ed54bce2c3d46ba5347eb5bc44910a
cj pfenninger <cjpf@charliejuliet.net>
2020-02-18 00:32:09 -0500

    More Modularization
    _store_domain
    creates domain entry if not exist
    _domain_exists
    checks to see if domain is already created
    _store_message
    creates message entry if not exist
    _message_exists
    checks to see if message is already created
    refactored parse_log accordingly

--- app/parse/jobs.py
+++ app/parse/jobs.py
@@ -16,53 +16,48 @@ def parse_log():
     for line in Pygtail("ess.log", paranoid=True):
         data = re.findall(r'\{.*\}', line)
         data = json.loads(data[0])
-        message_id = data['message_id']
-        account_id = data['account_id']
-        domain_id = data['domain_id']
-        src_ip = data['src_ip']
-        ptr_record = data['ptr_record']
-        hdr_from = data['hdr_from']
-        env_from = data['env_from']
-        hdr_to = data['hdr_to']
-        dst_domain = data['dst_domain']
-        size = data['size']
-        subject = data['subject']
-        timestamp = data['timestamp']
 
-        if _is_test_entry(account_id, domain_id):
+        if _is_test_entry(data['account_id'], data['domain_id']):
             continue
 
-        print("Checking for existing Account ID...({})".format(account_id))
-        if not Account.query.filter_by(account_id=account_id).first():
-            print("Account ID not found. Creating entry.")
-            a = Account(account_id=account_id)
-            db.session.add(a)
-
-        print("Checking for existing Domain ID...({})".format(domain_id))
-        if not Domain.query.filter_by(domain_id=domain_id).first():
-            print("Domain ID not found. Creating entry.")
-            d = Domain(domain_id=domain_id)
-            db.session.add(d)
-
-        print("Checking for existing Message ID...({})".format(message_id))
-        if not Message.query.filter_by(message_id=message_id).first():
-            print("Message ID not found. Creating entry.")
-            m = Message(
-                message_id=message_id,
-                account_id=account_id,
-                domain_id=domain_id,
-                src_ip=src_ip,
-                ptr_record=ptr_record,
-                hdr_from=hdr_from,
-                env_from=env_from,
-                hdr_to=hdr_to,
-                dst_domain=dst_domain,
-                size=size,
-                subject=subject,
-                timestamp=timestamp
-            )
-            db.session.add(m)
+        _store_account(data)
+        _store_domain(data)
+        _store_message(data)
         db.session.commit()
+
+
+def _store_message(data):
+    'Creates new Message entry if not already created.'
+    print("Checking for existing Message ID...({})".format(data['message_id']))
+    if not _message_exists(data['message_id']):
+        print("Message ID not found. Creating entry.")
+        m = Message(
+            message_id=data['message_id'],
+            account_id=data['account_id'],
+            domain_id=data['domain_id'],
+            src_ip=data['src_ip'],
+            ptr_record=data['ptr_record'],
+            hdr_from=data['hdr_from'],
+            env_from=data['env_from'],
+            hdr_to=data['hdr_to'],
+            dst_domain=data['dst_domain'],
+            size=data['size'],
+            subject=data['subject'],
+            timestamp=data['timestamp']
+        )
+        try:
+            db.session.add(m)
+        except Exception as e:
+            db.rollback()
+            print(e)  # TODO log exception
+
+
+def _message_exists(message_id):
+    'Checks to see if a Message already exists in the database.'
+    return True if Message.query.filter_by(message_id=message_id).first() \
+        else False
+
+
 def _store_account(data):
     'Creates new Account entry if not already created.'
     print("Checking for existing Account ID...({})".format(data['account_id']))
@@ -79,6 +74,25 @@ def _store_account(data):
 def _account_exists(account_id):
     'Checks to see if an Account already exists in the database.'
     return True if Account.query.filter_by(account_id=account_id).first() \
+        else False
+
+
+def _store_domain(data):
+    'Creates new Domain entry if not already created.'
+    print("Checking for existing Domain ID...({})".format(data['domain_id']))
+    if not _domain_exists(data['domain_id']):
+        print("Domain ID not found. Creating entry.")
+        d = Domain(domain_id=data['domain_id'])
+        try:
+            db.session.add(d)
+        except Exception as e:
+            db.rollback()
+            print(e)  # TODO log exception
+
+
+def _domain_exists(domain_id):
+    'Checks to see if a Domain already exists in the database.'
+    return True if Domain.query.filter_by(domain_id=domain_id).first() \
         else False
 
 
