90256fa86110f3bbc3a5caaf107d1112eb9d1145
cj pfenninger <cjpf@charliejuliet.net>
2020-02-18 13:15:11 -0500

    Logging setup

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,6 @@
 import os
 import logging
-from logging.handlers import SMTPHandler, RotatingFileHandler
+from logging.handlers import SMTPHandler, TimedRotatingFileHandler
 from flask import Flask
 from flask_login import LoginManager
 from flask_sqlalchemy import SQLAlchemy
@@ -73,19 +73,21 @@ def create_app(config_class):
     else:
         if not os.path.exists('logs'):
             os.mkdir('logs')
-        file_handler = RotatingFileHandler(
-            'logs/barracuda-syslog-tools.log', maxBytes=10240, backupCount=10)
-        file_handler.setFormatter(logging.Formatter(
-            '%(asctime)s %(levelname)s: %(message)s \
-                [in %(pathname)s:%(lineno)d]'))
-        file_handler.setLevel(logging.INFO)
-        app.logger.addHandler(file_handler)
-
-    if not app.config['JOB_CONFIG']:
-        app.logger.setLevel(logging.INFO)
-        app.logger.info('barracuda-syslog-tools startup')
-    else:
-        app.logger.info('creating parse_log app')
+        if not app.config['JOB_CONFIG']:
+            file_handler = TimedRotatingFileHandler(
+                'logs/barracuda-syslog-tools.log',
+                when='D',
+                interval=1,
+                backupCount=14)
+            file_handler.setFormatter(logging.Formatter(
+                '%(asctime)s %(levelname)s: %(message)s \
+                    [in %(pathname)s:%(lineno)d]'))
+            file_handler.setLevel(logging.INFO)
+            app.logger.addHandler(file_handler)
+            app.logger.setLevel(logging.INFO)
+            app.logger.info('barracuda-syslog-tools startup')
+        else:
+            app.logger.info('creating parse_log app')
 
     if app.config['SCHEDULER_API_ENABLED']:
         app.logger.info("Scheduler API Enabled.  Starting Scheduler...")
