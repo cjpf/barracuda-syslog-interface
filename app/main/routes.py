521e60bbcf3cf42d0aea4831c8d662c5e8728c38
cj pfenninger <cjpf@charliejuliet.net>
2020-02-18 19:29:49 -0500

    Rudimentary Message Log

--- app/main/routes.py
+++ app/main/routes.py
@@ -1,8 +1,9 @@
 from datetime import datetime
-from flask import render_template
+from flask import render_template, jsonify, url_for
 from flask_login import login_required, current_user
 from app import db
 from app.main import bp
+from app.models import Message
 
 
 @bp.before_request
@@ -17,5 +18,20 @@ def before_request():
 @bp.route('/index', methods=['GET'])
 @login_required
 def index():
-    'Home Page'
+    'Home Page featuring Email Statistics Graphs'
     return render_template('index.html', title='Dashboard')
+
+
+@bp.route('/messages')
+@bp.route('/messages/<int:page>')
+def messages(page=1):
+    messages = Message.query.order_by(
+        Message.timestamp.desc()).paginate(page, 15, False)
+    next_url = url_for('main.messages', page=messages.next_num) \
+        if messages.has_next else None
+    prev_url = url_for('main.messages', page=messages.prev_num) \
+        if messages.has_prev else None
+    return render_template('messages.html',
+                           messages=messages.items,
+                           next_url=next_url,
+                           prev_url=prev_url)
