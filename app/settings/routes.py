981118239557f3791ac685167122f553d13017e3
cj pfenninger <cjpf@charliejuliet.net>
2020-02-19 00:20:43 -0500

    guess calculator
    implemented _calculate_guess to return a list of guesses for unnamed domain ids

--- app/settings/routes.py
+++ app/settings/routes.py
@@ -1,10 +1,11 @@
 from datetime import datetime
-from flask import render_template, request, flash, redirect, url_for
+from flask import render_template, request, flash, redirect, url_for,\
+    current_app
 from flask_login import login_required, current_user
 from app import db
 from app.settings import bp
 from app.settings.forms import SetDomainForm
-from app.models import Domain
+from app.models import Domain, Message
 
 
 @bp.before_request
@@ -20,9 +21,37 @@ def before_request():
 def settings():
     'Settings Page for App configuration'
     domains = Domain.query.all()
+    guesses = _calculate_guess()
     return render_template('settings/settings.html',
                            title='Settings',
-                           domains=domains)
+                           domains=domains,
+                           guesses=guesses)
+
+
+def _calculate_guess():
+    '''
+    Returns list of all guesses for unnamed domains
+    '''
+    domains = Domain.query.filter_by(name=None).all()
+    destinations = {}
+    guesses = {}
+    for domain in domains:
+        messages = Message.query.filter_by(domain_id=domain.domain_id).all()
+        for message in messages:
+            if message.dst_domain in destinations:
+                destinations[message.dst_domain] += 1
+            else:
+                destinations[message.dst_domain] = 1
+        max = 0
+        for d in destinations.items():
+            if d[1] > max:
+                max = d[1]
+                guess = d[0]
+        current_app.logger.info(
+            'The best guess for domain name to associate with {} is {}'
+            .format(domain.domain_id, guess))
+        guesses[domain.domain_id] = guess
+    return guesses
 
 
 @bp.route('/settings/set_domain/<domain_id>', methods=['GET', 'POST'])
