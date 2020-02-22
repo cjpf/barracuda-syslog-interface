6661ab70531babfd65da76ea199a3599ce0264b3
cj pfenninger <cjpf@charliejuliet.net>
2020-02-21 23:57:28 -0500

    flask-httpauth
    BasicAuth and TokenAuth implementation

---
+++ app/api/auth.py
@@ -0,0 +1,46 @@
+from flask import g
+from flask_httpauth import HTTPBasicAuth, HTTPTokenAuth
+from app.models import User
+from app.api.errors import error_response
+
+basic_auth = HTTPBasicAuth()
+token_auth = HTTPTokenAuth()
+
+
+@basic_auth.verify_password
+def verify_password(email, password):
+    '''
+    True if credentials are valid
+    False if invalid
+    '''
+    user = User.query.filter_by(email=email).first()
+    if user is None:
+        return False
+    g.current_user = user
+    return user.check_password(password)
+
+
+@basic_auth.error_handler
+def basic_auth_error():
+    '''
+    Called when authentication fails
+    '''
+    return error_response(401)  # Unauthorized
+
+
+@token_auth.verify_token
+def verify_token(token):
+    '''
+    True if token found in db
+    False if token not found
+    '''
+    g.current_user = User.check_token(token) if token else None
+    return g.current_user is not None
+
+
+@token_auth.error_handler
+def token_auth_error():
+    '''
+    Called when token auth fails
+    '''
+    return error_response(401)  # Unauthorized
