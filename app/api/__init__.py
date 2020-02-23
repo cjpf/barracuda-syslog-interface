46784f228b0cd1f594b4685f78d8ceb775ea8440
cj pfenninger <cjpf@charliejuliet.net>
2020-02-23 13:59:59 -0500

    messages import to API blueprint

--- app/api/__init__.py
+++ app/api/__init__.py
@@ -2,4 +2,4 @@ from flask import Blueprint
 
 bp = Blueprint('api', __name__)
 
-from app.api import errors, tokens, users, domains, accounts
+from app.api import errors, tokens, users, domains, accounts, messages
